8eee3c9c9e0ddb7c36f398cfd93a210a53a453c4
abhinav <abhinavdutt19@gmail.com>
2017-11-20 19:14:55 +0530

    updated vote classifier

--- sentiments_training_program.py
+++ sentiments_training_program.py
@@ -7,6 +7,7 @@ from nltk.classify import ClassifierI
 import pickle
 
 from statistics import mode
+import statistics
 import re
 from nltk.tokenize import TweetTokenizer
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
@@ -23,18 +24,23 @@ class VoteClassifier(ClassifierI):
 		for c in self._classifiers:
 			v = c.classify(features)
 			votes.append(v)
-
-		return mode(votes)
+			print(v)
+		try:
+			return mode(votes)
+		except statistics.StatisticsError:
+			return 'positive'
 
 	def confidence(self, features):
 		votes = []
 		for c in self._classifiers:
 			v = c.classify(features)
 			votes.append(v)
-
-		choice_votes = votes.count(mode(votes))
-		conf = choice_votes/len(votes)
-		return conf
+		try:
+			choice_votes = votes.count(mode(votes))
+			conf = choice_votes/len(votes)
+			return conf
+		except statistics.StatisticsError:
+			return 1.0
 
 
 tknz = TweetTokenizer()
@@ -55,6 +61,7 @@ negative_tweets = twitter_samples.strings('negative_tweets.json')
 short_pos = open("positive.txt","r").read()
 short_neg = open("negative.txt","r").read()
 
+stop_words = stopwords.words('english')
 sentiment_tweets = []
 all_words = []
 
@@ -65,8 +72,9 @@ for p in short_pos.split('\n'):
 	words = word_tokenize(p)
 	pos = nltk.pos_tag(words)
 	for w in pos:
-	    if w[1][0] in allowed_word_types:
-	        all_words.append(w[0].lower())
+		if w[0].lower() not in stop_words:
+			if w[1][0] in allowed_word_types:
+				all_words.append(w[0].lower())
 
     
 for p in short_neg.split('\n'):
@@ -74,10 +82,9 @@ for p in short_neg.split('\n'):
 	words = word_tokenize(p)
 	pos = nltk.pos_tag(words)
 	for w in pos:
-		if w[1][0] in allowed_word_types:
-			all_words.append(w[0].lower())
-
-
+		if w[0].lower() not in stop_words:
+			if w[1][0] in allowed_word_types:
+				all_words.append(w[0].lower())
 
 positive_tweets_refined = []
 negative_tweets_refined = []
@@ -99,13 +106,12 @@ for i in range(len(positive_tweets)):
 	sentiment_tweets.append((positive_tweets[i],'positive'))
 
 for i in range(len(negative_tweets)):
-	sentiment_tweets.append((negative_tweets[i],'negetive'))
+	sentiment_tweets.append((negative_tweets[i],'negative'))
 
 random.shuffle(sentiment_tweets)
 
-stop_words = stopwords.words('english')
 
-tweets = positive_tweets + negative_tweets
+tweets =  positive_tweets+negative_tweets
 
 for tweet in tweets:
 	for word in tknz.tokenize(tweet):
@@ -114,9 +120,13 @@ for tweet in tweets:
 
 
 all_words = nltk.FreqDist(all_words)
-all_words = (all_words.most_common(5000))
-for word, freq in all_words:
-	print(word.encode('utf-8'),freq)
+# print(all_words['awesome'])
+all_words = (all_words.most_common(2000))
+# print(all_words['awesome'])
+
+
+# for word, freq in all_words:
+# 	print(word.encode('utf-8'),freq)
 
 word_features = [x[0] for x in all_words]
 
@@ -135,79 +145,79 @@ feature_set = [(find_features(tweet), category) for (tweet, category) in sentime
 # testing_set = feature_set[9000:] 
 # classifier.show_most_informative_features(25)
 
-MultinomialNB_classifier = SklearnClassifier(MultinomialNB())
-MultinomialNB_classifier.train(feature_set)
-# print('MultinomialNB classifier accuracy',nltk.classify.accuracy(MultinomialNB_classifier, testing_set))
-mnb_classifier_doc = open('mnb_classifier.pkl','wb')
-pickle.dump(MultinomialNB_classifier,mnb_classifier_doc)
-mnb_classifier_doc.close()
+# MultinomialNB_classifier = SklearnClassifier(MultinomialNB())
+# MultinomialNB_classifier.train(feature_set)
+# # print('MultinomialNB classifier accuracy',nltk.classify.accuracy(MultinomialNB_classifier, testing_set))
+# mnb_classifier_doc = open('mnb_classifier.pkl','wb')
+# pickle.dump(MultinomialNB_classifier,mnb_classifier_doc)
+# mnb_classifier_doc.close()
 
 # BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
 # BernoulliNB_classifier.train(feature_set)
-# print('BernoulliNB_classifier accuracy',nltk.classify.accuracy(BernoulliNB_classifier, testing_set))
+# # print('BernoulliNB_classifier accuracy',nltk.classify.accuracy(BernoulliNB_classifier, testing_set))
 # bernoulli_classifier_doc = open('bernoulli_classifier.pkl','wb')
 # pickle.dump(BernoulliNB_classifier,bernoulli_classifier_doc)
 # bernoulli_classifier_doc.close()
 
-LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-LogisticRegression_classifier.train(feature_set)
-# print('LogisticRegression classifier accuracy',nltk.classify.accuracy(LogisticRegression_classifier, testing_set))
-LogisticRegression_classifier_doc = open('LogisticRegression.pkl','wb')
-pickle.dump(LogisticRegression_classifier,LogisticRegression_classifier_doc)
-LogisticRegression_classifier_doc.close()
+# LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+# LogisticRegression_classifier.train(feature_set)
+# # print('LogisticRegression classifier accuracy',nltk.classify.accuracy(LogisticRegression_classifier, testing_set))
+# LogisticRegression_classifier_doc = open('LogisticRegression.pkl','wb')
+# pickle.dump(LogisticRegression_classifier,LogisticRegression_classifier_doc)
+# LogisticRegression_classifier_doc.close()
 
-SGDclassifier = SklearnClassifier(SGDClassifier())
-SGDclassifier.train(feature_set)
-# print('SGDClassifier classifier accuracy',nltk.classify.accuracy(SGDClassifier_classifier, testing_set))
-SGDClassifier_doc = open('SGDClassifier.pkl','wb')
-pickle.dump(SGDclassifier,SGDClassifier_doc)
-SGDClassifier_doc.close()
+# SGDclassifier = SklearnClassifier(SGDClassifier())
+# SGDclassifier.train(feature_set)
+# # print('SGDClassifier classifier accuracy',nltk.classify.accuracy(SGDClassifier_classifier, testing_set))
+# SGDClassifier_doc = open('SGDClassifier.pkl','wb')
+# pickle.dump(SGDclassifier,SGDClassifier_doc)
+# SGDClassifier_doc.close()
 
 # SVC_classifier = SklearnClassifier(SVC())
 # SVC_classifier.train(feature_set)
-# print('SVC classifier accuracy',nltk.classify.accuracy(SVC_classifier, testing_set))
+# # print('SVC classifier accuracy',nltk.classify.accuracy(SVC_classifier, testing_set))
 # SVC_classifier_doc = open('SVCClassifier.pkl','wb')
 # pickle.dump(SVC_classifier,SVC_classifier_doc)
 # SVC_classifier_doc.close()
 
-NuSVC_classifier = SklearnClassifier(NuSVC())
-NuSVC_classifier.train(feature_set)
-# print('NuSVC classifier accuracy',nltk.classify.accuracy(NuSVC_classifier, testing_set))
-NuSVC_classifier_doc = open('NuSVC_classifier.pkl','wb')
-pickle.dump(NuSVC_classifier,NuSVC_classifier_doc)
-NuSVC_classifier_doc.close()
+# NuSVC_classifier = SklearnClassifier(NuSVC())
+# NuSVC_classifier.train(feature_set)
+# # print('NuSVC classifier accuracy',nltk.classify.accuracy(NuSVC_classifier, testing_set))
+# NuSVC_classifier_doc = open('NuSVC_classifier.pkl','wb')
+# pickle.dump(NuSVC_classifier,NuSVC_classifier_doc)
+# NuSVC_classifier_doc.close()
 
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(feature_set)
-# print('LinearSVC classifier accuracy',nltk.classify.accuracy(LinearSVC_classifier, training_set))
-LinearSVC_classifier_doc = open('LinearSVC_classifier.pkl','wb')
-pickle.dump(LinearSVC_classifier,LinearSVC_classifier_doc)
-LinearSVC_classifier_doc.close()
+# LinearSVC_classifier = SklearnClassifier(LinearSVC())
+# LinearSVC_classifier.train(feature_set)
+# # print('LinearSVC classifier accuracy',nltk.classify.accuracy(LinearSVC_classifier, training_set))
+# LinearSVC_classifier_doc = open('LinearSVC_classifier.pkl','wb')
+# pickle.dump(LinearSVC_classifier,LinearSVC_classifier_doc)
+# LinearSVC_classifier_doc.close()
 
 # save_classifier = open('NaiveBayesClassifier.pickle', 'wb')
 # pickle.dump(classifier, save_classifier)
 # save_classifier.close()
 
-# with open('LinearSVC_classifier.pkl','rb') as fp:
-# 	LinearSVC_classifier = pickle.load(fp)
+with open('LinearSVC_classifier.pkl','rb') as fp:
+	LinearSVC_classifier = pickle.load(fp)
 	
-# with open('NuSVC_classifier.pkl','rb') as fp:
-# 	NuSVC_classifier = pickle.load(fp)
+with open('bernoulli_classifier.pkl','rb') as fp:
+	BernoulliNB_classifier = pickle.load(fp)
 	
-# with open('SGDClassifier.pkl','rb') as fp:
-# 	SGDclassifier = pickle.load(fp)
+with open('SGDClassifier.pkl','rb') as fp:
+	SGDclassifier = pickle.load(fp)
 
-# with open('mnb_classifier.pkl','rb') as fp:
-# 	MultinomialNB_classifier = pickle.load(fp)
+with open('mnb_classifier.pkl','rb') as fp:
+	MultinomialNB_classifier = pickle.load(fp)
 
-# with open('LogisticRegression.pkl','rb') as fp:
-# 	LogisticRegression_classifier = pickle.load(fp)
+with open('LogisticRegression.pkl','rb') as fp:
+	LogisticRegression_classifier = pickle.load(fp)
 
-voted_classifier = VoteClassifier(LinearSVC_classifier,
-								  NuSVC_classifier,
+voted_classifier = VoteClassifier(# NuSVC_classifier,
+								  BernoulliNB_classifier,
 								  SGDclassifier,
-								  MultinomialNB_classifier,
-								  LogisticRegression_classifier)
+								  MultinomialNB_classifier)
+								  # LogisticRegression_classifier)
 
 def sentiment(document):
 	feats = find_features(document)
--- twiter_code.py
+++ twiter_code.py
@@ -9,17 +9,20 @@ api_key = '2hIyn2HcjHm0sM4WQ6MJMkuLi'
 api_secret = 'gZtTPBqRc5wIg7JnbKigNyLxTZUI4guOC4MgXq2R47ndHk54Hz'
 access_token = '926503251385532416-LNXZXaUXdgF5kHRDfxBy3xVpUxCBmys'
 access_secret = 'wKxeZlFMER0FLBR9smxGH7VkL75rs4TIsVMe6GZaE7e0p'
-
-sentiments = []
-
+print(s.sentiment('The movie was horrible I will never watch such movie in my life'))
+print(s.sentiment('The best movie of the series,would definitely recommend it FIVE stars'))
+print(s.sentiment('very good movie,one of the best of all time'))
+# sentiments = []
+outpu_file = open('output_sentiments_for_ThorRagnorok.txt','wb')
 class Listener(StreamListener):
 
 	def on_data(self,data):
 		data = json.loads(data)
 		tweet = data['text'] 
 		sentiment_value,confidence = s.sentiment(tweet)
-		sentiments.append(sentiment_value)
+		# sentiments.append(sentiment_value)
 		print(tweet.encode('utf-8'),sentiment_value,confidence)
+
 		return True
 
 	def on_error(self,status):
@@ -30,5 +33,5 @@ auth.set_access_token(access_token,access_secret)
 
 twitterStream = Stream(auth,Listener())
 for x in range(100):
-	twitterStream.filter(track=['python'])
-print(sentiments)
+	twitterStream.filter(track=['Justice League'])
+# print(sentiments)
